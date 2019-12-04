cc78db9f6a6169a8523e3ed65365f41933c6cd5f
TimmyHung <58425985+TimmyHung@users.noreply.github.com>
2019-12-04 18:58:09 +0800

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -9,7 +9,8 @@ module.exports.run = async (bot, message, args) =>{
 
 let punishchannel =message.guild.channels.get("557512829327114250")
 
-if(!message.member.roles.some(["role.dcadmin","role.admin"])) return message.channel.send("[錯誤]權限不足");
+if(!message.member.roles.has(role.dcadmin) || !message.member.permissions.has("ADMINISTRATOR")) 
+        return message.channel.send("[錯誤]權限不足")
 if(message.member.roles.has(role.dcadmin) || message.channel.id != 557512829327114250) 
         return message.channel.send(`[錯誤]槓你的到正確的頻道使用啦 (${punishchannel})`)
 
