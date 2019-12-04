b866261b594d06eaee5299016394059bcb8193cf
TimmyHung <timmy0707@gmail.com>
2019-12-04 21:20:09 +0800

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -5,23 +5,26 @@ const superagent = require("superagent");
 const prefix = botconfig.prefix
 const ok = "✅";
 const no = "❌";
+const role = require("../role.json");
 
 module.exports.run = async (bot, message, args) =>{
-    let timmy = message.guild.members.get("490860337302601738")
-    let adminchannel = message.guild.channels.get("648476721200496670")
 
-    if(!message.member.hasPermission("KICK_MEMBERS"))
-        return message.channel.send(`[錯誤]你的權限不足，如有意願擔任DC小管理盡一份心力，歡迎私訊 ${timmy.user.tag}`)
+let punishchannel = message.guild.channels.get("648476721200496670")
 
-    if(message.channel.id != 648476721200496670)
-        return message.channel.send(`[錯誤]槓你的到正確的頻道使用啦(${adminchannel})`)
+if(message.member.roles.some(r=>[role.dcadmin, role.admin].includes(r.name))) return message.channel.send("[錯誤]權限不足");
+if(!message.member.roles.has(role.dcadmin)){ 
+} else if(message.channel.id != 648476721200496670)
+        return message.channel.send(`[錯誤]槓你的到正確的頻道使用啦 (${punishchannel})`)
     
     let accepter = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!accepter) return message.channel.send("[提示]未知用戶名，請確認你有正確 @用戶，或是 `!!help accept` 查看指令說明")
             .then(() => message.react('❌'))
 
     let playerRole = message.guild.roles.get(`557565545898049536`)
+    let newbieRole = message.guild.roles.get(`616469184905478160`)
+    
     accepter.addRole(playerRole)
+    accepter.removeRole(newbieRole)
 
     message.delete()
     message.channel.send(`用戶:${accepter.user.username} 的暱稱審核已通過。`).then(m => m.delete(5000));
