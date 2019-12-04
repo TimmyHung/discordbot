9417e52de869e843067297e449abb27daba27d36
TimmyHung <58425985+TimmyHung@users.noreply.github.com>
2019-12-04 17:13:06 +0800

    Add files via upload

---
+++ util/eventHandler.js
@@ -0,0 +1,9 @@
+const reqEvent = (event) => require(`../events/${event}`)
+
+module.exports = bot => {
+    bot.on("ready", function() {reqEvent("ready") (bot) });
+    bot.on("reconnecting", () => reqEvent("reconnecting") (bot))
+    bot.on("disconnecting", () => reqEvent("disconnecting") (bot))
+    bot.on("warn", reqEvent("warn"))
+    bot.on("error", reqEvent("error"))
+}
