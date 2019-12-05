8489278222736d61f4f144b6046269ac8894a08f
TimmyHung <timmy0707@gmail.com>
2019-12-06 00:59:36 +0800

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -7,7 +7,7 @@ const role = require("../role.json");
 
 module.exports.run = async (bot, message, args) =>{
     if(message.member.roles.some(r=>[role.dcadmin, role.admin].includes(r.name))) return message.channel.send("[錯誤]權限不足");
-    if(isNaN(args[0])) return message.channel.channel.send("[提示]請輸入要清除的訊息數量(1~100)");
+    if(isNaN(args[0])) return message.channel.send("[提示]請輸入要清除的訊息數量(1~100)");
     if (args[0] > 100) return message.channel.send("[錯誤]槓你的就說不要超過數字100了")
 
     message.channel.bulkDelete(args[0])
