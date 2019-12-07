d79848de72535495befa4e76393564b18e065968
TimmyHung <timmy0707@gmail.com>
2019-12-08 01:44:58 +0800

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -11,9 +11,7 @@ module.exports.run = async (bot, message, args) =>{
     let Membed = new Discord.RichEmbed()
     .setColor(colors.yellow)
     .setAuthor("該怎麼創建問題小房間", message.guild.iconURL)
-    .addField("想要直接取得官方協助又不想讓其他人知道?")
-    .addField("更換帳號、密碼遺失、外掛舉報卻懶得跑粉專?")
-    .addField("在下方輸入你的問題，我們會在最短時間內回覆你!")
+    .setDescription("想要取得最直接的官方協助又不想讓其他人知道?\n想要取得最直接的官方協助又不想讓其他人知道?\n在下方輸入你的問題，我們會在最短時間內回覆你!")
     .setFooter(`玩家&官方協助專區•由 ${timmy.user.tag} 開發`, bot.user.displayAvatarURL)
     message.channel.send(Membed)
 }
