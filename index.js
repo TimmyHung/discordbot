a29013451eb873b1c1725613312eaad98e10c370
TimmyHung <timmy0707@gmail.com>
2019-12-08 00:34:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ bot.on("message", async message =>{
         else if(message.content.toLowerCase() === '!solved')
         ticketchannel = message.guild.channels.find(channel => channel.name === '問題小房間-' + asknick) 
                 { 
-                if (message.channel.name != ticketchannel) return message.delete().then(() => message.channel.send("[錯誤]這並不是你的問題小房間"))
+                if (message.channel.id != ticketchannel.id) return message.delete().then(() => message.channel.send("[錯誤]這並不是你的問題小房間"))
                 message.channel.delete() 
                     .then(channel => {
                         console.log("刪除頻道 " + channel.name);
