d0f52da1ee6e7a9fc770cf7cd42a7015f5a210bf
TimmyHung <timmy0707@gmail.com>
2019-12-08 02:20:48 +0800

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) =>{
     .setColor(colors.yellow)
     .setAuthor("該怎麼創建問題小房間", message.guild.iconURL)
     .setDescription("想要取得最直接的官方協助又不想讓其他人知道?\n更換帳號、密碼問題、外掛回報，卻懶得跑粉專?\n在下方輸入你的問題，我們會在最短時間內回覆你!")
-    .addField("小提醒:", "詢問或發送無異議的問題、訊息，純粹好玩心態來鬧的\n封鎖五天做處分，請勿浪費回覆人員的時間!")
+    .addField("小提醒:", "詢問或發送無意義的問題、訊息，純粹好玩心態來鬧的\n封鎖五天做處分，請勿浪費回覆人員的時間!")
     .setFooter(`玩家&官方協助專區•由 ${timmy.user.tag} 開發`, bot.user.displayAvatarURL)
     message.channel.send(Membed)
 }
