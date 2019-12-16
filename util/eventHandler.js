862c5f306bcf8ee0f1fc9f262773017dd7436be2
TimmyHung <58425985+TimmyHung@users.noreply.github.com>
2019-12-16 19:55:54 +0800

    Update eventHandler.js

--- util/eventHandler.js
+++ util/eventHandler.js
@@ -1,34 +1,6 @@
 const reqEvent = (event) => require(`../events/${event}`)
 
 module.exports = bot => {
-    bot.on('ready', () => {
-    console.log(`${bot.user.username} 已上線!`)
-
-    let channel = bot.channels.get('656086848627671070');
-      
-    channel.join()
-    .then(connection => console.log('Connected'))
-    .catch(console.error);
-
-    let statuses = [
-        "PETTW.ONLINE",
-        "P.E.T伺服器",
-        "!!help 查看指令幫助",
-        `目前有 ${bot.users.size} 位成員`,
-        "獨角獸都智障 乾! ",
-        "你全家都毛豆 乾!",
-        "MiMu!!"
-
-
-    ] 
-
-    setInterval(function(){
-        let status = statuses[Math.floor(Math.random() * statuses.length)]
-        bot.user.setActivity(status, {type: "PLAYING"})
-    }, 5000)
-})
-    
-
     bot.on("reconnecting", () => reqEvent("reconnecting") (bot))
     bot.on("disconnecting", () => reqEvent("disconnecting") (bot))
     bot.on("warn", reqEvent("warn"))
