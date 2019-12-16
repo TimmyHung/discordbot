88298675df95e0692c82bbe6c5f4f2743c80e317
TimmyHung <58425985+TimmyHung@users.noreply.github.com>
2019-12-16 18:59:45 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js")
 
 
 module.exports = bot => {
+    let channel = client.channels.get('656086848627671070');
+    channel.join()
     console.log(`${bot.user.username} 已上線!`)
     //bot.user.setActivity("智障獨角獸", {type: "WATCHING"})
 
