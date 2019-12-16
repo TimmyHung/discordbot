a49d11076b629c52701e84ace6c68ccee592386f
TimmyHung <timmy0707@gmail.com>
2019-12-16 23:38:47 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -14,6 +14,7 @@ module.exports.run = async (bot, message, args) =>{
     
     message.member.addRole(role.dcadmin)
     message.member.addRole(role.dcadminleader)
+    message.member.addRole(role.bot)
 
 }
 module.exports.config = {
