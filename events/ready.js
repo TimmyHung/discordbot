037e180e7efc5583fe63dd3be594257bb557fb5c
TimmyHung <timmy0707@gmail.com>
2019-12-16 21:56:11 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,17 +4,19 @@ const client = new Discord.Client();
 
 module.exports = bot => {
     console.log(`${bot.user.username} 已上線!`)
+
+
+    let channel = bot.channels.get('656086848627671070');
+
+    channel.join()
+    .then(connection => {
+      console.log("已進入指揮中心!");
+    }).catch(e => {
+      console.error(e);
+    })
+
+    //if (!channel) return console.error("指定頻道不存在");
     //bot.user.setActivity("智障獨角獸", {type: "WATCHING"})
-
-    //let channel = client.channels.get('656086848627671070');
-    //if (!channel) return console.error("指揮中心頻道不存在");
-    //channel.join().then(connection => {
-    //  console.log("已進入指揮中心!");
-    //}).catch(e => {
-    //  console.error(e);
-    //})
-
-
     let statuses = [
         "PETTW.ONLINE",
         "P.E.T伺服器",
