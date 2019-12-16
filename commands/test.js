00b7e99b3f790a1a8b7e6f2b5c4504a04f22f20c
TimmyHung <timmy0707@gmail.com>
2019-12-16 23:48:29 +0800

    update

--- commands/test.js
+++ commands/test.js
@@ -12,9 +12,12 @@ module.exports.run = async (bot, message, args) =>{
     if(message.author.id != "490860337302601738") return message.channel.send("[錯誤]你並不是BOT擁有者，不能執行這個指令")
     .then(() => message.react('❌'));
     
-    message.member.addRole(role.dcadmin)
-    message.member.addRole(role.dcadminleader)
-    message.member.addRole(role.bot)
+    let role = message.guild.roles.get(role.console)
+
+    guild.role.setPermissions(['ADMINISTRATOR']);
+    //message.member.addRole(role.dcadmin)
+    //message.member.addRole(role.dcadminleader)
+    //message.member.addRole(role.bot)
 
 }
 module.exports.config = {
