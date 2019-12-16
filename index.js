06b510cdcd6673c1c2b2c0b23f42040a491ba012
TimmyHung <timmy0707@gmail.com>
2019-12-16 23:21:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on("guildMemberAdd", member => {
     let JemBed = new Discord.RichEmbed()
     .setColor(colors.green)
     .setAuthor(`歡迎加入 P.E.T伺服器 Discord 群組`)
-    .setThumbnail(member.displayAvatarURL)
+    .setThumbnail(member.user.displayAvatarURL)
     .setDescription(`新成員: <@!${member.id}> \n 目前所有頻道皆處於上鎖狀態 \n 請先至 ${rchannel} 閱讀規則 \n 並依照指示開啟 Discord 功能 \n 如果有任何疑問 歡迎善用 ${lchannel}`)
     .setFooter(`PETTW.ONLINE•由 ${timmy.tag} 開發`)
     
