f425d5b4ffdb7412de3f0f88b056e0b846838d10
TimmyHung <timmy0707@gmail.com>
2019-12-19 22:44:06 +0800

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -9,23 +9,23 @@ const reqrole = require("../role.json");
 
 module.exports.run = async (bot, message, args) =>{
     if(!message.member.roles.has(reqrole.admin)) return message.channel.send("[錯誤]權限不足")
-    if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("I don't have permission to perform this command.")
+    if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("[錯誤]我並沒有足夠的權限(MANAGE_ROLES)")
     
     
     let rMember = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0])
-    if(!rMember) return message.channel.send("Please provide a user to add a role too.")
+    if(!rMember) return message.channel.send("[錯誤]未知的用戶名稱")
     let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
-    if(!role) return message.channel.send("Please provide a role to add to said user.") 
+    if(!role) return message.channel.send("[錯誤]請提供一個身分組") 
     let reason = args.slice(2).join(" ")
-    if(!reason) return message.channel.send("Please provide a reason")
+    if(!reason) reason = "無原因可提供"
 
 
 
     if(rMember.roles.has(role.id)) {
-        return message.channel.send(`${rMember.displayName}, already has the role!`)
+        return message.channel.send(`[錯誤]${rMember.displayName}, 已經有這個身分組了!`).then(m => m.delete(5000))
     } else {
         await rMember.addRole(role.id).catch(e => console.log(e.message))
-        message.channel.send(`The role, ${role.name}, has been added to ${rMember.displayName}.`)
+        message.channel.send(`[訊息]身分組:${role.name} 以新增給用戶 ${rMember.displayName}`).then(m => m.delete(5000))
     }
 
     let embed = new Discord.RichEmbed()
