57f66c623a1c20ddc7432abd4c195b9161d597a3
TimmyHung <timmy0707@gmail.com>
2019-12-19 22:11:26 +0800

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -11,8 +11,9 @@ module.exports.run = async (bot, message, args) =>{
     if(!message.member.roles.has(reqrole.admin)) return message.channel.send("[錯誤]權限不足")
 
     let action = args.join(" ");
-    if(action != 'add') return message.channel.send("[錯誤]指令用法不正確，是要 add 或是 remove 身分組?")
-    else if(action != 'remove') return message.channel.send("[錯誤]指令用法不正確，是要 add 或是 remove 身分組?")
+    if(action = 'add'){
+    }else if(action = 'remove'){
+    }else return message.channel.send("[錯誤]指令用法不正確，是要 add 或是 remove 身分組?")
     
     let actionarg;
     let role = args.slice(1).join(" ");
