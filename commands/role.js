61323ff27268a76816df3d8c7ac39a9cda6c2c56
TimmyHung <timmy0707@gmail.com>
2019-12-19 22:32:33 +0800

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -9,19 +9,36 @@ const reqrole = require("../role.json");
 
 module.exports.run = async (bot, message, args) =>{
     if(!message.member.roles.has(reqrole.admin)) return message.channel.send("[錯誤]權限不足")
-
-    let rolemember = message.mentions.members.first() || message.guild.members.get(args[0])
-    if(!rolemember) return message.channel.send("[錯誤]未知的用戶名稱")
+    if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("I don't have permission to perform this command.")
     
-    let actionarg;
-    let role = args.join(" ").slice(1);
-    if(!role) return message.channel.send("[錯誤]請輸入身分組名稱")
-    let roleargs = message.guild.roles.find(`name`, role);
-    if(!roleargs) return message.channel.send("[錯誤]未知的身分組")
+    
+    let rMember = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0])
+    if(!rMember) return message.channel.send("Please provide a user to add a role too.")
+    let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
+    if(!role) return message.channel.send("Please provide a role to add to said user.") 
+    let reason = args.slice(2).join(" ")
+    if(!reason) return message.channel.send("Please provide a reason")
 
 
-    if(rolemember.roles.has(roleargs.id))
-    await(rolemember.addRole(roleargs.id))
+
+    if(rMember.roles.has(role.id)) {
+        return message.channel.send(`${rMember.displayName}, already has the role!`)
+    } else {
+        await rMember.addRole(role.id).catch(e => console.log(e.message))
+        message.channel.send(`The role, ${role.name}, has been added to ${rMember.displayName}.`)
+    }
+
+    let embed = new Discord.RichEmbed()
+    .setColor(colours.redlight)
+    .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+    .addField("Moderation:", "Addrole")
+    .addField("Mutee:", rMember.user.username)
+    .addField("Moderator:", message.author.username)
+    .addField("Reason:", reason)
+    .addField("Date:", message.createdAt.toLocaleString())
+    
+        let sChannel = message.guild.channels.find(c => c.name === "tut-modlogs")
+        sChannel.send(embed)
 
 }
 module.exports.config = {
