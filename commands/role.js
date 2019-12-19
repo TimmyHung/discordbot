5a53eea01cfcbb71b81c627ad29900d1d40354fc
TimmyHung <timmy0707@gmail.com>
2019-12-19 21:47:14 +0800

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -11,29 +11,19 @@ module.exports.run = async (bot, message, args) =>{
     if(!message.member.roles.has(role.admin)) return message.channel.send("[錯誤]權限不足")
 
     let action = args.join(" ");
+    if(!action) return message.channel.send("[錯誤]指令用法不正確，是要 add 或是 remove 身分組?")
     let actionarg;
     let role = args.slice(1).join(" ");
+    if(!role) return message.channel.send("[錯誤]未知的身分組名稱")
     let roleargs = message.guild.roles.find(name, role);
     let rolemember = message.mentions.members.first() || message.guild.members.get(args[0])
+    if(!rolemember) return message.channel.send("[錯誤]未知的用戶名稱")
 
-    message.delete()
-    if(!action) {
-        return message.channel.send("[錯誤]指令用法不正確，是要 add 或是 remove 身分組?")
-    }else 
-        if(action === add){
-            if(!role){
-                return message.channel.send("[錯誤]未知的身分組名稱")
-            }else if(!rolemember){
-                return message.channel.send("[錯誤]未知的用戶名稱")
-            }else rolemember.addRole(roleargs.id)
-        if(action === false){
-            if(!role){
-                return message.channel.send("[錯誤]未知的身分組名稱")
-            }else if(!rolemember){
-                return message.channel.send("[錯誤]未知的用戶名稱")
-            }else rolemember.removeRole(roleargs.id)
-        }
-        }
+        message.delete()
+        if(action === add)
+            return rolemember.addRole(roleargs.id)
+        if(action === false)
+            return rolemember.removeRole(roleargs.id)
         }
 
 
