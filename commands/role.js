52019b308b24f442deaecd42fdc5e4d796e2ec4a
TimmyHung <timmy0707@gmail.com>
2019-12-19 22:23:07 +0800

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -10,32 +10,25 @@ const reqrole = require("../role.json");
 module.exports.run = async (bot, message, args) =>{
     if(!message.member.roles.has(reqrole.admin)) return message.channel.send("[錯誤]權限不足")
 
-    let action = args.join("");
-    if(action == 'add'){
-    }else if(action == 'remove'){
-    }else return message.channel.send("[錯誤]指令用法不正確，是要 add 或是 remove 身分組?")
+    let rolemember = message.mentions.members.first() || message.guild.members.get(args[0])
+    if(!rolemember) return message.channel.send("[錯誤]未知的用戶名稱")
     
     let actionarg;
     let role = args.slice(1).join(" ");
     if(!role) return message.channel.send("[錯誤]請輸入身分組名稱")
     let roleargs = message.guild.roles.find(role => role.name, role);
     if(!roleargs) return message.channel.send("[錯誤]未知的身分組")
-    let rolemember = message.mentions.members.first() || message.guild.members.get(args[0])
-    if(!rolemember) return message.channel.send("[錯誤]未知的用戶名稱")
 
-        message.delete()
-        if(action === 'add')
-            return rolemember.addRole(roleargs.id)
-        if(action === 'remove')
-            return rolemember.removeRole(roleargs.id)
-        }
+
+    if(rolemember.roles.has(roleargs.id))
+    await(rolemember.addRole(roleargs.id))
 
 
 module.exports.config = {
-    name: "role",
-    aliases: ["role"],
-    usage: `${prefix}role <add/remove> <身分組> <@用戶>`,
-    description: "新增或移除某位用戶的身分組",
+    name: "addrole",
+    aliases: ["addrole"],
+    usage: `${prefix}role <@用戶> <身分組>`,
+    description: "新增用戶的身分組",
     noalias: "無指令縮寫",
     user: "`全能管理`"
 }
