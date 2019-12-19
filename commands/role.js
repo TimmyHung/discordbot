4f604c7d0228afa0505dfdc67f9b7a482be2e81c
TimmyHung <timmy0707@gmail.com>
2019-12-19 21:56:55 +0800

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -14,15 +14,16 @@ module.exports.run = async (bot, message, args) =>{
     if(!action) return message.channel.send("[錯誤]指令用法不正確，是要 add 或是 remove 身分組?")
     let actionarg;
     let role = args.slice(1).join(" ");
-    if(!role) return message.channel.send("[錯誤]未知的身分組名稱")
-    let roleargs = message.guild.roles.find(`name`, role);
+    if(!role) return message.channel.send("[錯誤]請輸入身分組名稱")
+    let roleargs = message.guild.roles.find(role => role.name, role);
+    if(!roleargs) = message.channel.send("[錯誤]未知的身分組")
     let rolemember = message.mentions.members.first() || message.guild.members.get(args[0])
     if(!rolemember) return message.channel.send("[錯誤]未知的用戶名稱")
 
         message.delete()
         if(action === 'add')
             return rolemember.addRole(roleargs.id)
-        if(action === 'false')
+        if(action === 'remove')
             return rolemember.removeRole(roleargs.id)
         }
 
