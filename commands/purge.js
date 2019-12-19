b9db9cf3882cb0229a2cc4f20676bfa5fc45a5df
TimmyHung <58425985+TimmyHung@users.noreply.github.com>
2019-12-19 20:09:27 +0800

    update

--- commands/purge.js
+++ commands/purge.js
@@ -6,8 +6,7 @@ const prefix = botconfig.prefix
 const role = require("../role.json");
 
 module.exports.run = async (bot, message, args) =>{
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("[錯誤]權限不足");
-    if(message.member.roles.some(r=>[role.dcadmin, role.admin].includes(r.name))) return message.channel.send("[錯誤]權限不足");
+    if(!message.member.roles.has(role.admin) || !message.member.roles.has(role.dcadmin)) return message.channel.send("[錯誤]權限不足");
     if(isNaN(args[0])) return message.channel.send("[提示]請輸入要清除的訊息數量(1~100)");
     if (args[0] > 100) return message.channel.send("[錯誤]槓你的就說不要超過數字100了")
 
