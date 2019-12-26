67639f7a00c5a0890ea83d04d034f8df24ee2400
TimmyHung <timmy0707@gmail.com>
2019-12-27 01:40:24 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -35,4 +35,43 @@ module.exports = bot => {
       var memberCountChannel = bot.channels.get("659674065857937408");
       memberCountChannel.setName(`📐用戶統計 ${memberCount}人`);
    }, 1000);
-}
+
+
+   //伺服器資訊Embed
+   setInterval(function () {
+    var request = require('request');
+    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort; //伺服器偵測用API
+    var mcIP = '114.35.249.143'; //主機IP
+    var bungeePort = 25565; // 分流port
+    var mainPort = 25568; // 主線port
+    var lobbyPort = 25568; // 大廳port
+    var skyblockPort = 25568; // 空島port
+    var prisonPort = 25568; // 監獄port
+    var testchannel = bot.guild.channels.get("649553237384495104") // 測試頻道ID
+
+    request(url, function(err, response, body) {
+      if(err) {
+          console.log(err);
+          return message.reply('[錯誤]無法取得伺服器狀態...');
+      }
+      body = JSON.parse(body);
+      let statue = "離線❌";
+      let player = "N/A";
+      let statEmbed = new Discord.RichEmbed()
+        .setColor(colors.darkblue)
+        .setAuthor(`分流狀態一覽表`, bot.guild.displayAvatarURL)
+        .setDescription(`**分流:** 主線\n**狀態:** ${statue}\n線上人數: ${player}`)
+      
+      if(body.online) {
+          status = "線上✅";
+          player = `${body.players.now} / ${body.players.max}`;
+      }
+      testchannel.send(statEmbed);
+ }, 1000);
+})
+
+
+
+
+
+} //結束
