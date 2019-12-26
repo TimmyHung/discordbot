fdbaa8ede6eeb0cc919ce880a8bbb82917d8c6b4
TimmyHung <timmy0707@gmail.com>
2019-12-27 02:20:10 +0800

    update

--- commands/test.js
+++ commands/test.js
@@ -9,15 +9,38 @@ const no = "❌";
 
 module.exports.run = async (bot, message, args) =>{
 
-    if(message.author.id != "490860337302601738") return message.channel.send("[錯誤]你並不是BOT擁有者，不能執行這個指令")
-    .then(() => message.react('❌'));
-    
-    let role = message.guild.roles.get(role.console)
+       //伺服器資訊Embed
+  // setInterval(function () {
+    var request = require('request');
+    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mainPort; //伺服器偵測用API
+    var mcIP = '114.35.249.143'; //主機IP
+    var bungeePort = 25565; // 分流port
+    var mainPort = 25568; // 主線port
+    var lobbyPort = 25568; // 大廳port
+    var skyblockPort = 25568; // 空島port
+    var prisonPort = 25568; // 監獄port
+    var testchannel = bot.channels.get("649553237384495104") // 測試頻道ID
 
-    guild.role.setPermissions(['ADMINISTRATOR']);
-    //message.member.addRole(role.dcadmin)
-    //message.member.addRole(role.dcadminleader)
-    //message.member.addRole(role.bot)
+    request(url, function(err, response, body) {
+      if(err) {
+          console.log(err);
+          return message.reply('[錯誤]無法取得伺服器狀態...');
+      }
+      body = JSON.parse(body);
+      let statue = "離線❌";
+      let player = "N/A";
+      let statEmbed = new Discord.RichEmbed()
+        .setColor(colors.darkblue)
+        .setAuthor(`伺服器分流狀態一覽表`)
+        .setDescription(`**分流:** 主線\n**狀態:** ${statue}\n線上人數: ${player}`)
+      
+      if(body.online) {
+          status = "線上✅";
+          player = `${body.players.now} / ${body.players.max}`;
+      }
+      testchannel.send(statEmbed);
+// }, 10000);
+})
 
 }
 module.exports.config = {
--- events/ready.js
+++ events/ready.js
@@ -37,40 +37,6 @@ module.exports = bot => {
    }, 1000);
 
 
-   //伺服器資訊Embed
-   setInterval(function () {
-    var request = require('request');
-    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mainPort; //伺服器偵測用API
-    var mcIP = '114.35.249.143'; //主機IP
-    var bungeePort = 25565; // 分流port
-    var mainPort = 25568; // 主線port
-    var lobbyPort = 25568; // 大廳port
-    var skyblockPort = 25568; // 空島port
-    var prisonPort = 25568; // 監獄port
-    var testchannel = bot.channels.get("649553237384495104") // 測試頻道ID
-
-    request(url, function(err, response, body) {
-      if(err) {
-          console.log(err);
-          return message.reply('[錯誤]無法取得伺服器狀態...');
-      }
-      body = JSON.parse(body);
-      let statue = "離線❌";
-      let player = "N/A";
-      let statEmbed = new Discord.RichEmbed()
-        .setColor(colors.darkblue)
-        .setAuthor(`伺服器分流狀態一覽表`)
-        .setDescription(`**分流:** 主線\n**狀態:** ${statue}\n線上人數: ${player}`)
-      
-      if(body.online) {
-          status = "線上✅";
-          player = `${body.players.now} / ${body.players.max}`;
-      }
-      testchannel.send(statEmbed);
- }, 10000);
-})
-
-
 
 
 
