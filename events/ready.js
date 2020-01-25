15100bfbe26c46fecb2ea2b46997a13de5589adf
TimmyHung <timmy0707@gmail.com>
2020-01-26 02:37:49 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -34,7 +34,7 @@ module.exports = bot => {
    setInterval(function () {
       var memberCount = guild.members.filter(member => !member.user.bot).size;  
       var memberCountChannel = bot.channels.get("659674065857937408");
-      memberCountChannel.setName(`📐用戶統計 ${memberCount}人`);
+      memberCountChannel.setName(`✎「📐」用戶統計 ${memberCount}人`);
    }, 1000);
 
 
